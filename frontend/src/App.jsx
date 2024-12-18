2f7552313c9d3764419dcd21e5f016b5dd75ac2a
Andre Silva <andrenations@gmail.com>
2024-12-17 23:19:33 -0500

    add a new component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import FetchApi from "./components/fetch/FetchApi";
+// import FetchApi from "./components/fetch/FetchApi";
+import RockPaperScissors from "./components/rockpaperscissors/RockPaperScissors";
 
 const App = () => {
   return (
     <div>
-      <FetchApi />
+      {/* <FetchApi /> */}
+      <RockPaperScissors />
     </div>
   );
 };
