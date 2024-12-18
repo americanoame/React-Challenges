9e9807a6df0da062ea74a04542234094931cde7a
Andre Silva <andrenations@gmail.com>
2024-12-18 01:31:59 -0500

    add logic to determine winner and update result state

--- frontend/src/components/rockpaperscissors/RockPaperScissors.jsx
+++ frontend/src/components/rockpaperscissors/RockPaperScissors.jsx
@@ -1,26 +1,37 @@
 import { useState } from "react";
 
 const RockPaperScissors = () => {
- const [playerChoice, setPrayerChoice] = useState(null);
- const [computerChice, setComputerChoice] = useState(null);
- const [result, setResult] = useState([]);
+  const [playerChoice, setPrayerChoice] = useState(null);
+  const [computerChice, setComputerChoice] = useState(null);
+  const [result, setResult] = useState([]);
 
- const possibleChoice = ["rock", "paper", "scissors"];
+  const possibleChoice = ["rock", "paper", "scissors"];
 
- const playerPick= (choice) => {
-    setPrayerChoice(choice)
- 
+  const HandlePlayerPick = (choice) => {
+    setPrayerChoice(choice);
 
-    const computerPick = possibleChoice[Math.floor(Math.random() * possibleChoice.length)];
+    const computerPick =
+      possibleChoice[Math.floor(Math.random() * possibleChoice.length)];
     setComputerChoice(setComputerChoice);
 
- }
+    determiWinnew(choice, computerPick)
+  };
 
- 
+  const determiWinner = (player, computer) => {
+    if (player === computer) {
+      setResult("it's a draw");
+    } else if (
+      (player === "rock" && computer === "scissors") ||
+      (player === "paper" && computer === "rock") ||
+      (player === "scissors" && computer === "paper")
+    ) {
+      setResult("You win!");
+    } else {
+      setResult("You lost!");
+    }
+  };
 
-  return (
-    <div>RockPaperScissors</div>
-  )
-}
+  return <div>RockPaperScissors</div>;
+};
 
-export default RockPaperScissors
+export default RockPaperScissors;
